d44fc07c18f1d4718fd8120ee52279f941a39d0f
gauthamsambath <gauthamsambat@gmail.com>
2019-10-03 02:40:14 +0530

    autowire-xml-branch

--- src/main/java/com/stackroute/domain/Application.java
+++ src/main/java/com/stackroute/domain/Application.java
@@ -1,17 +1,7 @@
 package com.stackroute.domain;
 
-import org.springframework.beans.factory.support.BeanDefinitionReader;
-import org.springframework.beans.factory.support.BeanDefinitionRegistry;
-import org.springframework.beans.factory.support.DefaultListableBeanFactory;
-import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
-import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.context.support.FileSystemXmlApplicationContext;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.FileSystemResource;
-
-import java.util.Arrays;
 
 /**
  * Hello world!
@@ -23,25 +13,21 @@ public class Application
     {
         System.out.println("\n"+"using Application context"+"\n");
         ApplicationContext applicationContext= new ClassPathXmlApplicationContext("beans.xml");
-        Movie movie= (Movie) applicationContext.getBean("movie1");
-        System.out.println("\n"+movie.getActor().toString()+"\n");
-//        ApplicationContext applicationContext1= new ClassPathXmlApplicationContext("beans.xml");
-        Movie movie1= (Movie) applicationContext.getBean("movie1");
-        System.out.println("\n"+movie1.getActor().toString()+"\n");
-        System.out.println("Checking if both are the same objects with scope:prototype");
-        System.out.println(movie1==movie);
-        Movie movie2= (Movie) applicationContext.getBean("movie");
-        System.out.println("\n"+movie.getActor().toString()+"\n");
-//        ApplicationContext applicationContext1= new ClassPathXmlApplicationContext("beans.xml");
-        Movie movie3= (Movie) applicationContext.getBean("movie");
-        System.out.println("\n"+movie1.getActor().toString()+"\n");
-        System.out.println("Checking if both are the same objects with scope:singleton");
-        System.out.println(movie2==movie3);
-        System.out.println("\n Demonstration of multiple ids 'movieA' and 'movieB' in the same movie bean \n");
-        Movie movie4= (Movie) applicationContext.getBean("movieA");
-        System.out.println("\n"+movie4.getActor().toString()+"\n");
-        Movie movie5= (Movie) applicationContext.getBean("movieB");
-        System.out.println("\n"+movie5.getActor().toString()+"\n");
+        Movie movie= (Movie) applicationContext.getBean("movie");
+        System.out.println("\n"+movie.getActorObject1().toString()+"\n");
+//        System.out.println("Ecxeption thrown during 'Bytype' autowire");
+//        try
+//            {
+//                Movie movie1= (Movie) applicationContext.getBean("movie1");
+//            }
+//        catch (Exception e)
+//            {
+//                System.out.println(e.toString());
+//            }
+        Movie movie1=(Movie) applicationContext.getBean("movie1");
+        System.out.println("\n"+movie1.getActorObject1().toString()+"\n");
+
+
     }
 
 }
--- src/main/java/com/stackroute/domain/Movie.java
+++ src/main/java/com/stackroute/domain/Movie.java
@@ -2,14 +2,25 @@ package com.stackroute.domain;
 
 public class Movie
     {
-        Actor actor;
+        Actor actorObject1;
 
-        public Movie(Actor actor) {
-            this.actor = actor;
+        public Movie() {
+            System.out.println("using default constructor");
         }
 
-        public Actor getActor() {
-            return actor;
+        public Movie(Actor actorObject1) {
+            this.actorObject1 = actorObject1;
+            System.out.println("using parametrized constructor");
+        }
+
+        public void setActorObject1(Actor actorObject1) {
+
+            this.actorObject1 = actorObject1;
+            System.out.println("\n Using Setter \n");
+        }
+
+        public Actor getActorObject1() {
+            return actorObject1;
         }
 
 
